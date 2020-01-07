233554193a58b96e8536679e3e97a8c41bac664f
SahiBalata <levitomer1996@gmail.com>
2020-01-07 13:31:31 +0200

    fixed Login

--- ClientSide/src/components/MainComps/Login.js
+++ ClientSide/src/components/MainComps/Login.js
@@ -6,89 +6,96 @@ import { setAdminLoggedIn } from "../../redux/setAdminLoggedIn";
 import LoginForm from "./LoginComps/LoginForm";
 
 class Login extends Component {
-  state = {
-    redirect: false
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to="/" />;
-    }
-  };
-
-  setRedirect = () => {
-    this.setState({
-      redirect: true
-    });
-  };
-
-  handleSubmit(e) {
-    e.preventDefault();
-    var userName = document.querySelector("#userLogin").value;
-    var password = document.querySelector("#userPass").value;
-    const { setIsLoggedIn } = this.props;
-
-    let RegDetails = {
-      userName,
-      password
+    state = {
+        redirect: false
     };
-    let regJSONED = JSON.stringify(RegDetails);
 
-    fetch("http://localhost:4000/users/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: regJSONED
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("HTTP error " + response.status);
-        } else if (response.status === 200) {
-          return response.text();
-        } else if (response.status === 401) {
-          return response.text();
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to = "/" / > ;
         }
-      })
-      .then(data => {
-        console.log(data);
-        let dataJson = JSON.parse(data);
-        sessionStorage.setItem("user-auth", dataJson.accessToken);
-        // setIsLoggedIn(true);
-        if (dataJson.decoded.admin) {
-          console.log("Decoded is admin");
-          // setAdminLoggedIn(true);
-          setIsLoggedIn(true, true, dataJson.decoded.name);
-        } else {
-          setIsLoggedIn(true, false, dataJson.decoded.name);
-        }
+    };
 
-        this.setRedirect();
-      })
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        });
+    };
 
-      .catch(error => {
-        throw error;
-      });
-  }
-  SayShlomi() {
-    console.log("Shlomi");
-  }
+    handleSubmit(e) {
+        e.preventDefault();
+        var userName = document.querySelector("#userLogin").value;
+        var password = document.querySelector("#userPass").value;
+        const { setIsLoggedIn } = this.props;
 
-  render() {
-    return (
-      <>
-        {this.renderRedirect()}
+        let RegDetails = {
+            userName,
+            password
+        };
+        let regJSONED = JSON.stringify(RegDetails);
 
-        <LoginForm submit={this.handleSubmit} />
-      </>
-    );
-  }
+        fetch("http://localhost:4000/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: regJSONED
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP error " + response.status);
+                } else if (response.status === 200) {
+                    return response.text();
+                } else if (response.status === 201) {
+
+                    return response.text()
+                }
+            })
+            .then(data => {
+                if (data === 'Password is incorrect') {
+                    alert(data)
+                    return
+                } else if (data === 'User is not exist') {
+                    alert(data)
+                    return
+                }
+                let dataJson = JSON.parse(data);
+
+                sessionStorage.setItem("user-auth", dataJson.accessToken);
+                // setIsLoggedIn(true);
+                if (dataJson.decoded.admin) {
+                    console.log("Decoded is admin");
+                    // setAdminLoggedIn(true);
+                    setIsLoggedIn(true, true, dataJson.decoded.name);
+                } else {
+                    setIsLoggedIn(true, false, dataJson.decoded.name);
+                }
+
+                this.setRedirect();
+            })
+
+        .catch(error => {
+            throw error;
+        });
+    }
+    SayShlomi() {
+        console.log("Shlomi");
+    }
+
+    render() {
+        return ( < > { this.renderRedirect() }
+
+            <
+            LoginForm submit = { this.handleSubmit }
+            /> </ >
+        );
+    }
 }
 
 export default connect(null, { setIsLoggedIn, setAdminLoggedIn })(Login);
--- server/routes/users.js
+++ server/routes/users.js
@@ -8,109 +8,109 @@ var jwt = require("jsonwebtoken");
 
 //SQL connectiion
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "vacations"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "vacations"
 });
 connection.connect();
 
 //Registration handlers
 function checkUserExist(user) {
-  return new Promise((reso, rej) => {
-    connection.query(
-      `SELECT username 
+    return new Promise((reso, rej) => {
+        connection.query(
+            `SELECT username 
     FROM users WHERE username = "${user}" `,
-      function(error, results, fields) {
-        if (results.length == 0) {
-          reso(true);
-        } else if (error) {
-          rej(error);
-        } else {
-          rej("User already exist");
-        }
-      }
-    );
-  });
+            function(error, results, fields) {
+                if (results.length == 0) {
+                    reso(true);
+                } else if (error) {
+                    rej(error);
+                } else {
+                    rej("User already exist");
+                }
+            }
+        );
+    });
 }
 
 function checkEmailExist(email) {
-  return new Promise((reso, rej) => {
-    connection.query(
-      `SELECT email 
+    return new Promise((reso, rej) => {
+        connection.query(
+            `SELECT email 
         FROM users WHERE email = "${email}" `,
-      function(error, results, fields) {
-        if (results.length == 0) {
-          console.log(results);
-          reso(true);
-        } else if (error) {
-          rej(error);
-        } else {
-          rej("Email already exist");
-        }
-      }
-    );
-  });
+            function(error, results, fields) {
+                if (results.length == 0) {
+                    console.log(results);
+                    reso(true);
+                } else if (error) {
+                    rej(error);
+                } else {
+                    rej("Email already exist");
+                }
+            }
+        );
+    });
 }
 
-router.post("/register", async (req, res) => {
-  try {
-    await checkUserExist(req.body.userName);
-    await checkEmailExist(req.body.email);
-    var hash = bcrypt.hashSync(req.body.password, salt);
-    let array = JSON.stringify([]);
-    let q = `INSERT INTO users (username, password, email, lastName, firstName, admin, vaca_followed)
+router.post("/register", async(req, res) => {
+    try {
+        await checkUserExist(req.body.userName);
+        await checkEmailExist(req.body.email);
+        var hash = bcrypt.hashSync(req.body.password, salt);
+        let array = JSON.stringify([]);
+        let q = `INSERT INTO users (username, password, email, lastName, firstName, admin, vaca_followed)
     VALUES ("${req.body.userName}", "${hash}", "${req.body.email}","${
       req.body.lastName
     }", "${req.body.firstName}", ${false}, '${array}');`;
-    connection.query(q, function(error, results, fields) {
-      if (error) throw error;
-      res.status(200).send(`${req.body.userName} was created`);
-    });
-  } catch (err) {
-    res.status(201).send(err);
-  }
+        connection.query(q, function(error, results, fields) {
+            if (error) throw error;
+            res.status(200).send(`${req.body.userName} was created`);
+        });
+    } catch (err) {
+        res.status(201).send(err);
+    }
 });
 
 //Login handelers
 function checkUser(user, password) {
-  return new Promise((reso, rej) => {
-    let q = `SELECT password, admin
+    return new Promise((reso, rej) => {
+        let q = `SELECT password, admin
         FROM users WHERE username = "${user}"`;
-    connection.query(q, function(error, results, fields) {
-      if (error) throw error;
-      console.log(results);
-      if (results.length === 0) {
-        rej("User is not exist");
-      } else if (!bcrypt.compareSync(password, results[0].password)) {
-        rej("Password is incorrect");
-      } else if (results[0].admin === 1) {
-        console.log("Admin connected");
-        reso(true);
-      } else {
-        reso(false);
-      }
+        connection.query(q, function(error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            if (results.length === 0) {
+                rej("User is not exist");
+            } else if (!bcrypt.compareSync(password, results[0].password)) {
+                rej("Password is incorrect");
+            } else if (results[0].admin === 1) {
+                console.log("Admin connected");
+                reso(true);
+            } else {
+                reso(false);
+            }
+        });
     });
-  });
 }
 
-router.post("/login", async (req, res) => {
-  try {
-    let CU = await checkUser(req.body.userName, req.body.password);
-    const user = { name: req.body.userName, admin: CU };
-    const accessToken = jwt.sign(user, "tomer");
-    var decoded = jwt.verify(accessToken, "tomer");
+router.post("/login", async(req, res) => {
+    try {
+        let CU = await checkUser(req.body.userName, req.body.password);
+        const user = { name: req.body.userName, admin: CU };
+        const accessToken = jwt.sign(user, "tomer");
+        var decoded = jwt.verify(accessToken, "tomer");
 
-    let outPutObj = { accessToken: accessToken, decoded: decoded };
-    res.status(200).send(outPutObj);
-  } catch (err) {
-    res.status(401).send(err);
-  }
+        let outPutObj = { accessToken: accessToken, decoded: decoded };
+        res.status(200).send(outPutObj);
+    } catch (err) {
+        res.status(201).send(err);
+    }
 });
 
 router.post("/token", (req, res) => {
-  var decoded = jwt.verify(req.body.token, "tomer");
-  res.send(decoded);
+    var decoded = jwt.verify(req.body.token, "tomer");
+    res.send(decoded);
 });
 
 module.exports = router;
